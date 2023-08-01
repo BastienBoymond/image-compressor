f15da74e89e1d16e7973eb81141228321a1f3a5b
Bastien <bboymond@numalis.com>
2023-08-01 11:43:20 +0200

    create fix for clean

---
+++ clean.sh
@@ -0,0 +1,8 @@
+killall imageCompressor
+killall stack
+find ./tests -name "*.jpg" -delete
+find ./tests -name "*.png" -delete
+find ./tests -name "ImgToTxt.txt" -delete
+find ./tests -name "newCompressedImage" -delete
+find ./api -name "*.jpg" -delete
+find ./api -name "*.png" -delete
